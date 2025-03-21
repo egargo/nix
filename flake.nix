e19cc231f8cc5a687e7ed609be0e21b1d9b20f3f
Clint Egargo <67180945+egargo@users.noreply.github.com>
2025-03-21 20:00:12 +0800

    Update

--- flake.nix
+++ flake.nix
@@ -5,24 +5,24 @@
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 	};
 
-	outputs = { self, nixpkgs }: let
+	outputs = { self, nixpkgs }:
+	let
 		system = "x86_64-linux";
 		pkgs = import nixpkgs {
 			inherit system;
-
 			config = {
 				allowUnfree = true;
 			};
 		};
-	in {
-		nixosConfigurations.bee = nixpkgs.lib.nixosSystem {
-			specialArgs = {
-				inherit system;
+	in
+	{
+		nixosConfigurations = {
+			bee = nixpkgs.lib.nixosSystem {
+				specialArgs = { inherit system; };
+				modules = [
+					./configuration.nix
+				];
 			};
-
-			modules = [
-				./configuration.nix
-			];
 		};
 	};
 }
