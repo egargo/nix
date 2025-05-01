8f11d143d14a62c6aef1e7ea69b541693dca52d6
Clint Egargo <67180945+egargo@users.noreply.github.com>
2025-05-01 19:13:31 +0800

    update

--- configuration.nix
+++ configuration.nix
@@ -154,6 +154,7 @@
 			__NV_PRIME_RENDER_OFFLOAD = "1";
 			__NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
 			__GLX_VENDOR_LIBRARY_NAME = "nvidia";
+			__GLX_RENDERER = "nvidia";
 			__VK_LAYER_NV_optimus = "NVIDIA_only";
 
 			VDPAU_DRIVER="nvidia";
@@ -164,10 +165,8 @@
 			MOZ_DISABLE_RDD_SANDBOX = "1";
 			WLR_NO_HARDWARE_CURSORS = "1";
 			NVD_BACKEND = "direct";
-			NIXOS_OZONE_WL = "1";
 			MUTTER_ALLOW_HYBRID_GPUS = "1";
 			MOZ_ENABLE_WAYLAND = "1";
-			__GLX_RENDERER = "nvidia";
 		};
 	};
 
