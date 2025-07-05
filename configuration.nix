0531843744b9c47cf06a181dc3e03b35bfade491
Clint Egargo <67180945+egargo@users.noreply.github.com>
2025-07-05 20:40:06 +0800

    update

--- configuration.nix
+++ configuration.nix
@@ -5,303 +5,280 @@
 { config, pkgs, inputs, ... }:
 
 {
-	imports = [
-		./hardware-configuration.nix
-	];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-	# Bootloader.
-	boot = {
-		loader = {
-			systemd-boot.enable = true;
-			efi.canTouchEfiVariables = true;
-		};
-	};
+  # Bootloader.
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
 
-	networking.hostName = "bee";
-	# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "bee";
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-	# Configure network proxy if necessary
-	# networking.proxy.default = "http://user:password@proxy:port/";
-	# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-	# Enable networking
-	networking.networkmanager.enable = true;
+  # Enable networking
+  networking.networkmanager.enable = true;
 
-	# Set your time zone.
-	time.timeZone = "Asia/Manila";
+  # Set your time zone.
+  time.timeZone = "Asia/Manila";
 
-	# Select internationalisation properties.
-	i18n = {
-		defaultLocale = "en_US.UTF-8";
-		extraLocaleSettings = {
-			LC_ADDRESS = "en_US.UTF-8";
-			LC_IDENTIFICATION = "en_US.UTF-8";
-			LC_MEASUREMENT = "en_US.UTF-8";
-			LC_MONETARY = "en_US.UTF-8";
-			LC_NAME = "en_US.UTF-8";
-			LC_NUMERIC = "en_US.UTF-8";
-			LC_PAPER = "en_US.UTF-8";
-			LC_TELEPHONE = "en_US.UTF-8";
-			LC_TIME = "en_US.UTF-8";
-		};
-	};
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+      extraLocaleSettings = {
+        LC_ADDRESS = "en_US.UTF-8";
+        LC_IDENTIFICATION = "en_US.UTF-8";
+        LC_MEASUREMENT = "en_US.UTF-8";
+        LC_MONETARY = "en_US.UTF-8";
+        LC_NAME = "en_US.UTF-8";
+        LC_NUMERIC = "en_US.UTF-8";
+        LC_PAPER = "en_US.UTF-8";
+        LC_TELEPHONE = "en_US.UTF-8";
+        LC_TIME = "en_US.UTF-8";
+    };
+  };
 
-	services = {
-		# Flatpak
-		# flatpak.enable = true;
+  services = {
+    # Flatpak
+    # flatpak.enable = true;
 
-		# GNOME.
-		desktopManager.gnome.enable = true;
-		displayManager = {
-			defaultSession = "gnome";
-			gdm.enable = true;
-		};
-		gnome.gcr-ssh-agent.enable = true;
+    # GNOME.
+    desktopManager.gnome.enable = true;
+      displayManager = {
+        defaultSession = "gnome";
+        gdm.enable = true;
+    };
+    gnome.gcr-ssh-agent.enable = true;
 
-		# Enable sound.
-		pipewire = {
-			enable = true;
-			alsa.enable = true;
-			alsa.support32Bit = true;
-			pulse.enable = true;
-			jack.enable = true;
+    # Enable sound.
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
 
-			# use the example session manager (no others are packaged yet so this is enabled by default,
-			# no need to redefine it in your config for now)
-			#media-session.enable = true;
-		};
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
 
-		# channel:unstable
-		pulseaudio.enable = false;
+    # channel:unstable
+    pulseaudio.enable = false;
 
-		# Enable touchpad support (enabled default in most desktopManager).
-		libinput.enable = true;
+    # Enable touchpad support (enabled default in most desktopManager).
+    libinput.enable = true;
 
-		# Enable CUPS to print documents.
-		printing.enable = true;
-		avahi = {
-			enable = true;
-			nssmdns4 = true;
-			openFirewall = true;
-		};
+    # Enable CUPS to print documents.
+    printing.enable = true;
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
 
-		# resolved = {
-		# 	enable = true;
-		# 	dnssec = "true";
-		# 	domains = [ "~." ];
-		# 	fallbackDns = [
-		# 		"1.1.1.3#one.one.one.one"
-		# 		"1.0.0.3#one.one.one.one"
-		# 		"2606:4700:4700::1113#one.one.one.one"
-		# 		"2606:4700:4700::1003#one.one.one.one"
-		# 	];
-		# 	dnsovertls = "true";
-		# };
+    # resolved = {
+    #   enable = true;
+    #   dnssec = "true";
+    #   domains = [ "~." ];
+    #   fallbackDns = [
+    #     "1.1.1.3#one.one.one.one"
+    #     "1.0.0.3#one.one.one.one"
+    #     "2606:4700:4700::1113#one.one.one.one"
+    #     "2606:4700:4700::1003#one.one.one.one"
+    #   ];
+    #   dnsovertls = "true";
+    # };
 
-		# Enable the X11 windowing system.
-		xserver = {
-			enable = true;
-			excludePackages = with pkgs; [ xterm ];
-			videoDrivers = [ "nvidia" ];
-			xkb = {
-				layout = "us";
-				variant = "";
-			};
-		};
+    # Enable the X11 windowing system.
+    xserver = {
+      enable = true;
+      excludePackages = with pkgs; [ xterm ];
+      videoDrivers = [ "nvidia" ];
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
 
-		hardware.openrgb = {
-			enable = true;
-			motherboard = "amd";
-		};
+    hardware.openrgb = {
+      enable = true;
+      motherboard = "amd";
+    };
 
-		teamviewer.enable = true;
-	};
+    teamviewer.enable = true;
+  };
 
-	environment = {
-		gnome.excludePackages = with pkgs; [
-			epiphany
-			geary
-			gnome-connections
-			gnome-contacts
-			gnome-console
-			gnome-font-viewer
-			gnome-logs
-			gnome-maps
-			gnome-music
-			gnome-photos
-			gnome-software
-			gnome-tour
-			gnome-weather
-			seahorse
-			totem
-			yelp
-		];
+  environment = {
+    gnome.excludePackages = with pkgs; [
+      epiphany
+      geary
+      gnome-connections
+      gnome-contacts
+      gnome-console
+      gnome-font-viewer
+      gnome-logs
+      gnome-maps
+      gnome-music
+      gnome-photos
+      gnome-software
+      gnome-tour
+      gnome-weather
+      seahorse
+      totem
+      yelp
+    ];
 
-		systemPackages = with pkgs; [
+    systemPackages = with pkgs; [
       home-manager
-			brave
-			# dbeaver-bin
-			# gcc
-			ghostty
-			# gimp3
-			# gnumake
-			# go
-			# lazydocker
-			# lua
-			# nodejs
-			# libreoffice
-			# netflix
-			# python313
-			# postman
-			# qbittorrent
-			# stow
-			# vscode-fhs
-			# wget
-		];
+      brave
+      # dbeaver-bin
+      ghostty
+    ];
 
-		variables.EDITOR = "nvim";
+    variables.EDITOR = "nvim";
 
-		sessionVariables = {
-			__NV_PRIME_RENDER_OFFLOAD = "1";
-			__NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
-			__GLX_VENDOR_LIBRARY_NAME = "nvidia";
-			__GLX_RENDERER = "nvidia";
-			__VK_LAYER_NV_optimus = "NVIDIA_only";
+    sessionVariables = {
+      __NV_PRIME_RENDER_OFFLOAD = "1";
+      __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      __GLX_RENDERER = "nvidia";
+      __VK_LAYER_NV_optimus = "NVIDIA_only";
 
-			VDPAU_DRIVER="nvidia";
-			DRI_PRIME="pci-0000_01_00_0";
-			EGL_PLATFORM = "wayland";
-			MOZ_DRM_DEVICE="/run/opengl-driver/lib/dri/nvidia_drv_video.so";
-			GBM_BACKEND = "nvidia-drm";
-			MOZ_DISABLE_RDD_SANDBOX = "1";
-			WLR_NO_HARDWARE_CURSORS = "1";
-			NVD_BACKEND = "direct";
-			MUTTER_ALLOW_HYBRID_GPUS = "1";
-			MOZ_ENABLE_WAYLAND = "1";
-		};
-	};
+      VDPAU_DRIVER="nvidia";
+      DRI_PRIME="pci-0000_01_00_0";
+      EGL_PLATFORM = "wayland";
+      MOZ_DRM_DEVICE="/run/opengl-driver/lib/dri/nvidia_drv_video.so";
+      GBM_BACKEND = "nvidia-drm";
+      MOZ_DISABLE_RDD_SANDBOX = "1";
+      WLR_NO_HARDWARE_CURSORS = "1";
+      NVD_BACKEND = "direct";
+      MUTTER_ALLOW_HYBRID_GPUS = "1";
+      MOZ_ENABLE_WAYLAND = "1";
+    };
+  };
 
-	hardware = {
-		bluetooth = {
-			enable = true;
-			powerOnBoot = false;
-			settings = {
-				General = {
-					Experimental = false;
-				};
-			};
-		};
-		graphics = {
-			enable = true;
-			extraPackages = with pkgs; [
-				nvidia-vaapi-driver
-			];
-		};
-		nvidia = {
-			forceFullCompositionPipeline = true;
-			modesetting.enable = true;
-			powerManagement.enable = true;
-			powerManagement.finegrained = false;
-			open = false;
-			nvidiaSettings = true;
-			package = config.boot.kernelPackages.nvidiaPackages.production;
-			prime = {
-				# offload = {
-				# 	enable = true;
-				# 	enableOffloadCmd = true;
-				# };
-				sync.enable = true;
-				nvidiaBusId = "PCI:1:0:0";
-				amdgpuBusId = "PCI:5:0:0";
-			};
-			# channel:unstable
-			videoAcceleration = true;
-			gsp.enable = true;
-			nvidiaPersistenced = true;
-		};
-		nvidia-container-toolkit.enable = true;
-		# channel:stable
-		# pulseaudio = {
-		# 	enable = false;
-		# 	package = with pkgs; [ pulseaudioFull ];
-		# };
-	};
+  hardware = {
+    bluetooth = {
+      enable = true;
+      powerOnBoot = false;
+      settings = {
+        General = {
+          Experimental = false;
+        };
+      };
+    };
+    graphics = {
+      enable = true;
+      extraPackages = with pkgs; [
+        nvidia-vaapi-driver
+      ];
+    };
+    nvidia = {
+      forceFullCompositionPipeline = true;
+      modesetting.enable = true;
+      powerManagement.enable = true;
+      powerManagement.finegrained = false;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.production;
+      prime = {
+        # offload = {
+        #   enable = true;
+        #   enableOffloadCmd = true;
+        # };
+        sync.enable = true;
+        nvidiaBusId = "PCI:1:0:0";
+        amdgpuBusId = "PCI:5:0:0";
+      };
+      # channel:unstable
+      videoAcceleration = true;
+      gsp.enable = true;
+      nvidiaPersistenced = true;
+    };
+    nvidia-container-toolkit.enable = true;
+    # channel:stable
+    # pulseaudio = {
+    #   enable = false;
+    #   package = with pkgs; [ pulseaudioFull ];
+    # };
+  };
 
-	powerManagement.enable = true;
+  powerManagement.enable = true;
 
-	security.rtkit.enable = true;
+  security.rtkit.enable = true;
 
-	# Define a user account. Don't forget to set a password with ‘passwd’.
-	users = {
-		defaultUserShell = pkgs.zsh;
-		users.clint = {
-			isNormalUser = true;
-			home = "/home/clint";
-			description = "Clint";
-			extraGroups = [
-				"docker"
-				"jackaudio"
-				"networkmanager"
-				"video"
-				"wheel"
-			];
-			shell = pkgs.zsh;
-			packages = with pkgs; [];
-		};
-	};
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users = {
+    defaultUserShell = pkgs.zsh;
+    users.clint = {
+      isNormalUser = true;
+      home = "/home/clint";
+      description = "Clint";
+      extraGroups = [
+        "docker"
+        "jackaudio"
+        "networkmanager"
+        "video"
+        "wheel"
+      ];
+      shell = pkgs.zsh;
+      packages = with pkgs; [];
+    };
+  };
 
+  fonts.packages = with pkgs; [
+    # channel:stable
+    # (nerdfonts.override {
+    #   fonts = [ "Iosevka" ];
+    # })
 
-	# home-manager = {
-	# 	users = {
-	# 		clint = import ./home.nix;
-	# 	};
-	# 	extraSpecialArgs = { inherit pkgs inputs; };
-	# };
+    # channel:unstable
+    nerd-fonts.iosevka
+  ];
 
-	fonts.packages = with pkgs; [
-		# channel:stable
-		# (nerdfonts.override {
-		# 	fonts = [ "Iosevka" ];
-		# })
+  programs = {
+    fzf.fuzzyCompletion = true;
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      ohMyZsh = {
+        enable = true;
+        plugins = [
+          "command-not-found"
+          "docker"
+          "fzf"
+          "git"
+          "git-prompt"
+          "gitignore"
+          "golang"
+          "rust"
+          "safe-paste"
+          "shrink-path"
+          "vi-mode"
+          "zsh-interactive-cd"
+          "zsh-navigation-tools"
+        ];
+        theme = "robbyrussell";
+      };
+      syntaxHighlighting.enable = true;
+      autosuggestions.enable = true;
+    };
+  };
 
-		# channel:unstable
-		nerd-fonts.iosevka
-	];
-
-	programs = {
-		fzf.fuzzyCompletion = true;
-		zsh = {
-			enable = true;
-			enableCompletion = true;
-			ohMyZsh = {
-				enable = true;
-				plugins = [
-					"command-not-found"
-					"docker"
-					"fzf"
-					"git"
-					"git-prompt"
-					"gitignore"
-					"golang"
-					"rust"
-					"safe-paste"
-					"shrink-path"
-					"vi-mode"
-					"zsh-interactive-cd"
-					"zsh-navigation-tools"
-				];
-				theme = "robbyrussell";
-			};
-			syntaxHighlighting.enable = true;
-			autosuggestions.enable = true;
-		};
-	};
-
-	# channel:unstable
-	# systemd.services = {
-	# 	"getty@tty1".enable = false;
-	# 	"autovt@tty1".enable = false;
-	# };
+  # channel:unstable
+  # systemd.services = {
+  #   "getty@tty1".enable = false;
+  #   "autovt@tty1".enable = false;
+  # };
 
   virtualisation = {
     docker = {
@@ -312,47 +289,46 @@
     };
   };
 
-	# documentation.nixos.enable = true;
-	nix = {
-		gc = {
-			automatic = true;
-			dates = "daily";
-			options = "--delete-older-than 7d";
-		};
-		settings = {
-			auto-optimise-store = true;
-			experimental-features = [ "nix-command" "flakes" ];
-		};
-	};
+  # documentation.nixos.enable = true;
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 7d";
+    };
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+  };
 
-	# Allow unfree packages
-	nixpkgs.config.allowUnfree = true;
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-	# Some programs need SUID wrappers, can be configured further or are
-	# started in user sessions.
-	# programs.mtr.enable = true;
-	# programs.gnupg.agent = {
-	#   enable = true;
-	#   enableSSHSupport = true;
-	# };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-	# List services that you want to enable:
+  # List services that you want to enable:
 
-	# Enable the OpenSSH daemon.
-	# services.openssh.enable = true;
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
 
-	# Open ports in the firewall.
-	# networking.firewall.allowedTCPPorts = [ ... ];
-	# networking.firewall.allowedUDPPorts = [ ... ];
-	# Or disable the firewall altogether.
-	# networking.firewall.enable = false;
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
-	# This value determines the NixOS release from which the default
-	# settings for stateful data, like file locations and database versions
-	# on your system were taken. It‘s perfectly fine and recommended to leave
-	# this value at the release version of the first install of this system.
-	# Before changing this value read the documentation for this option
-	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	system.stateVersion = "24.11"; # Did you read the comment?
-
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
 }
--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -4,45 +4,45 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-	imports = [
-		(modulesPath + "/installer/scan/not-detected.nix")
-	];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
-	boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
-	boot.initrd.kernelModules = [ ];
-	boot.kernelModules = [ "kvm-amd" ];
-	boot.extraModulePackages = [ ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
 
-	fileSystems."/" = {
-		device = "/dev/disk/by-label/nixos";
-		fsType = "ext4";
-	};
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
 
-	fileSystems."/boot" = {
-		device = "/dev/disk/by-label/boot";
-		fsType = "vfat";
-		options = [ "fmask=0077" "dmask=0077" ];
-	};
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/boot";
+    fsType = "vfat";
+    options = [ "fmask=0077" "dmask=0077" ];
+  };
 
-	fileSystems."/mnt/JAJS600M2TB" = {
-		device = "/dev/disk/by-path/pci-0000:06:00.0-ata-2-part1";
-		fsType = "ext4";
-	};
+  fileSystems."/mnt/JAJS600M2TB" = {
+    device = "/dev/disk/by-path/pci-0000:06:00.0-ata-2-part1";
+    fsType = "ext4";
+  };
 
-	swapDevices = [
-		{
-			device = "/dev/disk/by-label/swap";
-		}
-	];
+  swapDevices = [
+    {
+      device = "/dev/disk/by-label/swap";
+    }
+  ];
 
-	# Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-	# (the default) this is the recommended approach. When using systemd-networkd it's
-	# still possible to use this option, but it's recommended to use it in conjunction
-	# with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-	networking.useDHCP = lib.mkDefault true;
-	# networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
-	# networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
 
-	nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-	hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
